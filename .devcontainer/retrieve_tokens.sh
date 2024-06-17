a24b64b3ee6158479e6190c01116d4af0459cecd
Maverick Wong <mvrck.wong@gmail.com>
2024-06-18 01:55:00 +0800

    update

--- .devcontainer/retrieve_tokens.sh
+++ .devcontainer/retrieve_tokens.sh
@@ -1,25 +1,30 @@
 #!/bin/sh
 
+# Source the environment from the current directory.
+source .env
+
 # Retrieve the API token
 HCP_API_TOKEN=$(curl --location "https://auth.idp.hashicorp.com/oauth2/token" \
 --header "Content-Type: application/x-www-form-urlencoded" \
---data-urlencode "client_id=1lHczZGZEsVjRsRaYWI2Z9Vqt3zHPulD" \
---data-urlencode "client_secret=sqX1PvI5zgVqRbUxZ_GIlWpDuhuviSd3w-xRE5zvB3FiVfGADb-my1X0XfogKOMn" \
+--data-urlencode "client_id=$HCP_CLIENT_ID" \
+--data-urlencode "client_secret=$HCP_CLIENT_SECRET" \
 --data-urlencode "grant_type=client_credentials" \
 --data-urlencode "audience=https://api.hashicorp.cloud" | jq -r .access_token)
 
-# Run the main script or command (replace with your actual command)
-echo "Starting Airflow with API token..."
-echo "HCP_API_TOKEN: $HCP_API_TOKEN"
+# Check if the HCP_API_TOKEN has a value
+if [ -n "$HCP_API_TOKEN" ]; then
+  echo "HCP_API_TOKEN has value."
+else
+  echo "HCP_API_TOKEN is empty."
+fi
 
-# Get the database credentials
-AIRFLOW_DB_USER=$(curl -s --header 'X-Vault-Token: root' --request GET http://vault:8200/v1/secret/data/database | jq -r '.data.data.AIRFLOW_DB_USER');
-AIRFLOW_DB_PW=$(curl -s --header 'X-Vault-Token: root' --request GET http://vault:8200/v1/secret/data/database | jq -r '.data.data.AIRFLOW_DB_PW');
-AIRFLOW_DB_NAME=$(curl -s --header 'X-Vault-Token: root' --request GET http://vault:8200/v1/secret/data/database | jq -r '.data.data.AIRFLOW_DB_NAME');
-AIRFLOW_DB_HOST=$(curl -s --header 'X-Vault-Token: root' --request GET http://vault:8200/v1/secret/data/database | jq -r '.data.data.AIRFLOW_DB_HOST');
+# Fetch and parse the secrets
+response=$(curl --silent \
+  --location "https://api.cloud.hashicorp.com/secrets/2023-06-13/organizations/68da4ae2-5f9a-43aa-8c4e-046da0b3b20b/projects/55184cdf-03ba-42c3-a8e3-9bb96aa0d10c/apps/dev/open" \
+  --request GET \
+  --header "Authorization: Bearer $HCP_API_TOKEN" | jq -r '.secrets[] | "\(.name)=\(.version.value)"')
 
-# Export the credentials
-export AIRFLOW_DB_USER AIRFLOW_DB_PW AIRFLOW_DB_NAME AIRFLOW_DB_HOST;
-
-# Start Airflow services
-exec "$@"
+# Parse the response and assign to environment variables
+for line in $response; do
+  export $line
+done
